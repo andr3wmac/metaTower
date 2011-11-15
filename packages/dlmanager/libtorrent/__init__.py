a29aae912395d3549ca4c896698b0e94b8ab5d37
Andrew MacIntyre <andrewmac@gmail.com>
2011-11-14 21:45:16 -0400

    libtorrent

--- packages/dlmanager/libtorrent/__init__.py
+++ packages/dlmanager/libtorrent/__init__.py
@@ -1,5 +1,6 @@
 import ctypes, os
 
+libboost_python = None
 libboost_thread = None
 libboost_system = None
 libboost_filesystem = None
@@ -8,10 +9,12 @@ libtorrent_rasterbar = None
 libtorrent = None
 
 def getLibrary():
-    global libboost_thread, libboost_system, libboost_filesystem, libboost_system, libtorrent_rasterbar, libtorrent
+    global libboost_python, libboost_thread, libboost_system, libboost_filesystem, 
+        libboost_system, libtorrent_rasterbar, libtorrent
 
     if ( not libtorrent ):
         path = os.path.dirname(__file__)
+        libboost_python = ctypes.CDLL(os.path.join(path, "libboost_python-py27.so.1.46.1"))
         libboost_thread = ctypes.CDLL(os.path.join(path, "libboost_thread.so.1.46.1"))
         libboost_system = ctypes.CDLL(os.path.join(path, "libboost_system.so.1.46.1"))
         libboost_filesystem = ctypes.CDLL(os.path.join(path, "libboost_filesystem.so.1.46.1"))
