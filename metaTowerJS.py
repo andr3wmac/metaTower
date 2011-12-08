f8cfd61cb6c6e09a67199dec89cd0c0bdcec13ea
Andrew MacIntyre <andrewmac@gmail.com>
2011-12-08 15:29:29 -0400

    v0.4.0

--- metaTowerJS.py
+++ metaTowerJS.py
@@ -194,15 +194,4 @@ mt.error = function(err)
 {
     alert("An error occured." + err);
 };
-
-mt.clearCookies = function() {
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
-}
 """
