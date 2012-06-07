d19dcce34f5aadb00d69245fc09c4a135cd5555e
Andrew Mac <andrewmac@gmail.com>
2012-06-06 21:37:32 -0300

    jmanlite test

--- packages/jmanlite/jmanlite.js
+++ packages/jmanlite/jmanlite.js
@@ -1,34 +1,34 @@
 var jmanlite = {
     menu_items: [],
-	MenuItem: function()
-	{
-		this.caption = "";
-		this.package_name = "";
-		this.on_click = "";
-	},
+    MenuItem: function()
+    {
+        this.caption = "";
+        this.package_name = "";
+        this.on_click = "";
+    },
 
-	menu: function(caption, package_name)
-	{
-		var item = new this.MenuItem();
-		item.caption = caption;
-		item.package_name = package_name;
-		this.menu_items.push(item);
+    menu: function(caption, package_name)
+    {
+        var item = new this.MenuItem();
+        item.caption = caption;
+        item.package_name = package_name;
+        this.menu_items.push(item);
 
-		mt.html("jmanlite_taskbar", "<li><a onClick=\"jmanlite.menuClicked('" + package_name + "');\" href=\"#\">" + caption + "</a></li>", true);
-	},
+        mt.html("jmanlite_taskbar", "<li><a onClick=\"jmanlite.menuClicked('" + package_name + "');\" href=\"#\">" + caption + "</a></li>", true);
+    },
 
-	menuClicked: function(package_name)
-	{
-		for ( i = 0; i < this.menu_items.length; i++ )
-		{
-			var entry = this.menu_items[i];
-			if ( entry.package_name == package_name )
-			{
-				mt("event('jmanlite.menu." + package_name + "')")
-                entry.style.font-weight = "bold";
-			} else {
-                entry.style.font-weight = "normal";
+    menuClicked: function(package_name)
+    {
+        for ( i = 0; i < this.menu_items.length; i++ )
+        {
+            var entry = this.menu_items[i];
+            if ( entry.package_name == package_name )
+            {
+                mt("event('jmanlite.menu." + package_name + "')")
+                entry.style.fontWeight = "bold";
+            } else {
+                entry.style.fontWeight = "normal";
             }
-		}
-	}
+        }
+    }
 };
