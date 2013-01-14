0398db347d7c9eb501e07816121386bd18be5ac6
andrewmac <andrew.mac@gmail.com>
2013-01-14 13:54:58 -0400

    dlmanager fix

--- packages/dlmanager/__init__.py
+++ packages/dlmanager/__init__.py
@@ -112,63 +112,72 @@ def update(resp):
 
     # gather data from NZB queue.
     for nzb in nzb_queue:
-        filename = os.path.basename(nzb.filename).replace("'", "\'")   
+        try:
+            filename = os.path.basename(nzb.filename).replace("'", "\'")   
 
-        # already deleted.
-        if ( nzb.removed ):
-            resp.js("dlmanager.nzb('" + nzb.uid + "', '" + filename + "', -1);") 
+            # already deleted.
+            if ( nzb.removed ):
+                resp.js("dlmanager.nzb('" + nzb.uid + "', '" + filename + "', -1);") 
 
-        # downloading
-        elif ( nzb.downloading ) and ( nzb_engine != None ) and ( nzb_engine.running ):
-            status = nzb_engine.execute("getStatus")
+            # downloading
+            elif ( nzb.downloading ) and ( nzb_engine != None ) and ( nzb_engine.running ):
+                status = nzb_engine.execute("getStatus")
 
-            # State 1: currently downloading
-            if ( not status.assembly ):
-                args = {"total": int(status.total_bytes/1048576), 
-                        "completed": int(status.current_bytes/1048576),
-                        "percent": round(status.current_bytes/float(status.total_bytes)*100),
-                        "dl_rate": status.kbps}
-                resp.js("dlmanager.nzb('" + nzb.uid + "', '" + filename + "', 1, " + str(args) + ");")
+                # State 1: currently downloading
+                if ( not status.assembly ):
+                    args = {"total": int(status.total_bytes/1048576), 
+                            "completed": int(status.current_bytes/1048576),
+                            "percent": round(status.current_bytes/float(status.total_bytes)*100),
+                            "dl_rate": status.kbps}
+                    resp.js("dlmanager.nzb('" + nzb.uid + "', '" + filename + "', 1, " + str(args) + ");")
 
-            # State 2: being assembled.
+                # State 2: being assembled.
+                else:
+                    args = {"assembly_percent": status.assembly_percent}
+                    resp.js("dlmanager.nzb('" + nzb.uid + "', '" + filename + "', 2, " + str(args) + ");")
+
+            # State 3: failed
+            elif ( nzb.error ):
+                resp.js("dlmanager.nzb('" + nzb.uid + "', '" + filename + "', 3);")
+
+            # State 4: completed
+            elif ( nzb.completed ):
+                args = {"par2": nzb.par2_results, 
+                        "unrar": nzb.unrar_results}
+                resp.js("dlmanager.nzb('" + nzb.uid + "', '" + filename + "', 4, " + str(args) + ");")
+
+            # State 0: queued
             else:
-                args = {"assembly_percent": status.assembly_percent}
-                resp.js("dlmanager.nzb('" + nzb.uid + "', '" + filename + "', 2, " + str(args) + ");")
-
-        # State 3: failed
-        elif ( nzb.error ):
-            resp.js("dlmanager.nzb('" + nzb.uid + "', '" + filename + "', 3);")
-
-        # State 4: completed
-        elif ( nzb.completed ):
-            args = {"par2": nzb.par2_results, 
-                    "unrar": nzb.unrar_results}
-            resp.js("dlmanager.nzb('" + nzb.uid + "', '" + filename + "', 4, " + str(args) + ");")
-
-        # State 0: queued
-        else:
-            resp.js("dlmanager.nzb('" + nzb.uid + "', '" + filename + "', 0);")
+                resp.js("dlmanager.nzb('" + nzb.uid + "', '" + filename + "', 0);")
+        except:
+            mt.log.error("Error getting status update for nzb.")
+            pass
 
     # Gather data from torrent queue.
     for torrent in torrent_queue:
-        # Torrent Removed.
-        if ( torrent.removed ):
-            resp.js("dlmanager.torrent('" + torrent.uid + "', '" + os.path.basename(torrent.filename) + "', -1);")
+        try:
+            # Torrent Removed.
+            if ( torrent.removed ):
+                resp.js("dlmanager.torrent('" + torrent.uid + "', '" + os.path.basename(torrent.filename) + "', -1);")
 
-        # Torrent is inactive.
-        elif ( torrent.lt_entry == None ):
-            resp.js("dlmanager.torrent('" + torrent.uid + "', '" + os.path.basename(torrent.filename) + "', 0);")
+            # Torrent is inactive.
+            elif ( torrent.lt_entry == None ):
+                resp.js("dlmanager.torrent('" + torrent.uid + "', '" + os.path.basename(torrent.filename) + "', 0);")
 
-        # Torrent is in an active state.
-        else:
-            status = torrent.lt_entry.status()
-            state_str = ['Queued.', 'Checking..', 'Downloading metadata..', 'Downloading..', 'Finished.', 'Seeding.', 'Allocating..']
-            args = {"msg": state_str[status.state],
-                    "progress": status.progress * 100,
-                    "dl_rate": round(status.download_rate / 1000, 2),
-                    "ul_rate": round(status.upload_rate / 1000, 2),
-                    "peers": status.num_peers}
-            resp.js("dlmanager.torrent('" + torrent.uid + "', '" + os.path.basename(torrent.filename) + "', 1, " + str(args) + ");")
+            # Torrent is in an active state.
+            else:
+                status = torrent.lt_entry.status()
+                state_str = ['Queued.', 'Checking..', 'Downloading metadata..', 'Downloading..', 'Finished.', 'Seeding.', 'Allocating..']
+                args = {"msg": state_str[status.state],
+                        "progress": status.progress * 100,
+                        "dl_rate": round(status.download_rate / 1000, 2),
+                        "ul_rate": round(status.upload_rate / 1000, 2),
+                        "peers": status.num_peers}
+                resp.js("dlmanager.torrent('" + torrent.uid + "', '" + os.path.basename(torrent.filename) + "', 1, " + str(args) + ");")
+
+        except:
+            mt.log.error("Error getting status update for torrent.")    
+            pass
 
     # Trigger an update for next time.
     resp.js("dlmanager.update();")
